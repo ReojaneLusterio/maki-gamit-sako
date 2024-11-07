b1eb5122ddedf5d02967c92e7752c90bcd58dc36
ReojaneLusterio <145820016+ReojaneLusterio@users.noreply.github.com>
2024-11-06 22:43:12 -0800

    Add files via upload

---
+++ shit/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>my first ubuntu server php developemnt</title>
+</head>
+<body>
+
+  <input type="text" name="name">
+  <input type="text" name="middle name">
+  <input type="text" name="last name">
+  <input type="text" name="age">
+  <input type="text" name="address">
+  <input type="text" name="crouse and section">
+
+</body>
+</html>
